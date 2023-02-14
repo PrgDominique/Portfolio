f8e686b16ec47ca4c3e3bace4870d7f1d48711d4
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-14 21:25:53 +0800

    refactor code

--- src/hooks/useColorMode.tsx
+++ src/hooks/useColorMode.tsx
@@ -5,25 +5,22 @@ export const useColorMode = () => {
 
   useEffect(() => {
     const color = localStorage.getItem('color-mode')
-    if (color === null) {
-      localStorage.setItem('color-mode', 'light')
-    } else {
-      setColorMode(color)
-      const className = 'dark'
-      const bodyClasses = window.document.body.classList
+    color === null
+      ? localStorage.setItem('color-mode', 'light')
+      : setColorMode(color)
+    const className = 'dark'
+    const bodyClasses = window.document.body.classList
 
-      color === 'dark'
-        ? bodyClasses.add(className)
-        : bodyClasses.remove(className)
-    }
-  }, [])
+    color === 'dark'
+      ? bodyClasses.add(className)
+      : bodyClasses.remove(className)
+  }, [colorMode])
 
   const toggleColor = () => {
-    if (colorMode === 'light') {
-      localStorage.setItem('color-mode', 'dark')
-    } else {
-      localStorage.setItem('color-mode', 'light')
-    }
+    colorMode === 'light'
+      ? localStorage.setItem('color-mode', 'dark')
+      : localStorage.setItem('color-mode', 'light')
+
     setColorMode(colorMode === 'light' ? 'dark' : 'light')
     const className = 'dark'
     const bodyClasses = window.document.body.classList

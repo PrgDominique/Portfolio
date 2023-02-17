8a75d0124d56688fceff88399f73de7fe09ae1fe
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-17 12:44:16 +0800

    added active class for every render page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,55 @@
 import { useColorMode } from '@/hooks/useColorMode'
-import Link from 'next/link'
-import { useState } from 'react'
+import { useRouter } from 'next/router'
 import { ToggleButton } from './ui/button/ToggleButton'
 import { ListItem } from './ui/navbar/ListItem'
 
 const Navbar = () => {
   const [colorMode, toggleColor] = useColorMode()
+  const router = useRouter()
+
+  // get the current path name from router object
+  const currentPathname = router.pathname
 
   return (
     <div className='bg-[#212529] p-6'>
       <div className='flex md:justify-end gap-5 text-white text-xl'>
         <ul className='flex gap-5'>
           <li>
-            <ListItem href='/home' name='Home' />
+            <ListItem
+              href='/home'
+              name='Home'
+              active={currentPathname === '/home'}
+            />
           </li>
           <li>
-            <ListItem href='/about' name='About' />
+            <ListItem
+              href='/about'
+              name='About'
+              active={currentPathname === '/about'}
+            />
           </li>
           <li>
-            <ListItem href='/skill' name='Skills' />
+            <ListItem
+              href='/skill'
+              name='Skills'
+              active={currentPathname === '/skill'}
+            />
           </li>
           <li>
-            <ListItem href='/project' name='Projects' />
+            <ListItem
+              href='/project'
+              name='Projects'
+              active={currentPathname === '/project'}
+            />
           </li>
           <li>
-            <ListItem href='#' name='Resume' />
+            <ListItem
+              href='#'
+              name='Resume'
+              active={currentPathname === '/resume'}
+            />
           </li>
           <li>
-            {' '}
             <ToggleButton colorMode={colorMode} toggleColor={toggleColor} />
           </li>
         </ul>
--- src/components/ui/navbar/ListItem.tsx
+++ src/components/ui/navbar/ListItem.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link'
 
-export const ListItem = ({href, name}: any) => {
+export const ListItem = ({ href, name, active }: any) => {
   return (
     <div>
-        <Link href={href}>{name}</Link>
+      <Link
+        href={href}
+        className={
+          active
+            ? 'hover:text-slate-700 text-slate-700 font-bold'
+            : 'hover:text-slate-700'
+        }
+      >
+        {name}
+      </Link>
     </div>
   )
 }
-

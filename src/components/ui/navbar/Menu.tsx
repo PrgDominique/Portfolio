b62b6795625d45b139e4eb591c2f425d2972e0ab
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-18 19:21:06 +0800

    added X icon with logic useState

--- src/components/ui/navbar/Menu.tsx
+++ src/components/ui/navbar/Menu.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
 
-const Menu = () => {
+const Menu = ({setMenuActive, menuActive}: any) => {
+ 
   return (
     <div>
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-        viewBox='0 0 24 24'
-        strokeWidth='1.5'
-        stroke='currentColor'
-        className='w-6 h-6'
-      >
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          d='M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5'
-        />
-      </svg>
+      {menuActive ? (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='currentColor'
+          className='w-10 h-10'
+          onClick={() => setMenuActive(!menuActive)}
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5'
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 20 20'
+          fill='currentColor'
+          className='w-10 h-10'
+          onClick={() => setMenuActive(!menuActive)}
+        >
+          <path d='M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z' />
+        </svg>
+      )}
     </div>
   )
 }

bb0dfc457c302a3d2c3127bd935b185fde96e7b9
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-19 12:54:12 +0800

    added transition duration

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import Navbar from '../Navbar'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='dark:bg-[#121212] h-screen '>
+    <div className='dark:bg-[#121212] transition duration-300 ease-in h-screen text-[#121212] '>
       <Navbar />
       <div >
         <div className='p-5'>{children}</div>

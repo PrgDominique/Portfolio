d2e7f7f8b557110da1990d4d841b2254dd24e047
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-19 13:05:59 +0800

    cleanup

--- src/components/home/LinkedIn.tsx
+++ src/components/home/LinkedIn.tsx
@@ -6,7 +6,7 @@ export const LinkedIn = () => {
         <img
           src='https://cdn-icons-png.flaticon.com/512/174/174857.png'
           alt='linkedIn'
-          className='w-14 h-14 border rounded-lg '
+          className='w-14 h-14 border rounded-lg'
         />
       </a>
     </div>

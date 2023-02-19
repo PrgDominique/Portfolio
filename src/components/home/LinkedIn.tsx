91cb1e8fdf627cf6c1d451d2baa3a5c9f1ba2a1b
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-19 12:53:42 +0800

    added LinkedIn icon and link

---
+++ src/components/home/LinkedIn.tsx
@@ -0,0 +1,14 @@
+export const LinkedIn = () => {
+  return (
+    <div>
+      {' '}
+      <a target='_blank' href='https://www.linkedin.com/in/dominique-simbillo/'>
+        <img
+          src='https://cdn-icons-png.flaticon.com/512/174/174857.png'
+          alt='linkedIn'
+          className='w-14 h-14 border rounded-lg '
+        />
+      </a>
+    </div>
+  )
+}

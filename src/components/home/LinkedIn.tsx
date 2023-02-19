aed5ecbb1fbcb292adcc906bffff4f4c3b669da4
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-19 13:16:39 +0800

    added noopener

--- src/components/home/LinkedIn.tsx
+++ src/components/home/LinkedIn.tsx
@@ -2,7 +2,11 @@ export const LinkedIn = () => {
   return (
     <div>
       {' '}
-      <a target='_blank' href='https://www.linkedin.com/in/dominique-simbillo/'>
+      <a
+        target='_blank'
+        href='https://www.linkedin.com/in/dominique-simbillo/'
+        rel='noopener noreferrer'
+      >
         <img
           src='https://cdn-icons-png.flaticon.com/512/174/174857.png'
           alt='linkedIn'

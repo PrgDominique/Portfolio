61d6e471edceb9926f60f8590e07adbd317349b9
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-22 15:10:06 +0800

    Refactor About

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
-import DomzPhoto from '../assets/photo1.jpg'
-import Image from 'next/image'
+import Contents from '../localdata/about/Contents.json'
+import uuid from 'react-uuid'
 
 const About = () => {
   return (
@@ -7,32 +7,16 @@ const About = () => {
       <div className='md:mt-[8rem] mt-[5rem]'>
         <div className='grid md:grid-cols-2 gap-16'>
           <div className='row order-2 md:order-1'>
-            <div className='flex p-5'>
-              <h1 className='text-lg text-justify font-medium '>
-                Hello, I'm Dominique Simbillo, a software engineer with a focus
-                on web development. I received my Bachelor's degree in
-                Information Technology from Colegio De San Gabriel Archangel{' '}
-              </h1>
-            </div>
-            <div className='flex mt-5 p-5'>
-              <h1 className='text-lg text-justify font-medium '>
-                I have experience with front-end technologies such as HTML, CSS,
-                and JavaScript, as well as with ReactJS, one of the most popular
-                front-end JavaScript frameworks. I've also worked with NextJS, a
-                popular React framework for server-side rendering and building
-                static websites. I have a little knowledge of TypeScript, a
-                superset of JavaScript that provides static typing, making the
-                development of large applications easier and more reliable.{' '}
-              </h1>
-            </div>
-            <div className='flex mt-5 p-5'>
-              <h1 className='text-lg text-justify font-medium'>
-                On the back-end, I've worked with PHP and have experience with
-                Laravel, one of the most popular PHP frameworks. I enjoy
-                building RESTful APIs and integrating front-end applications
-                with back-end services.{' '}
-              </h1>
-            </div>
+            {Contents &&
+              Contents.map((content) => {
+                return (
+                  <div key={uuid()} className='flex p-5 mt-5'>
+                    <h1 className='text-lg text-justify font-medium '>
+                      {content.text}
+                    </h1>
+                  </div>
+                )
+              })}
           </div>
           <div className=' max-md:5-10 max-md:w-[300px] mt-10 flex md:order-2 order-1  mx-auto'>
             <img

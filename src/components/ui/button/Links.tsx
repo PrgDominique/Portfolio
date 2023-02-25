5765883101e6c24cfed34d0aea477015fa984722
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-25 12:18:17 +0800

    disable rules for no img element

--- src/components/ui/button/Links.tsx
+++ src/components/ui/button/Links.tsx
@@ -1,7 +1,7 @@
 export const Links = ({ src, name }: any) => {
   return (
     <div>
-      <a href={src} target='_blank'>
+      <a href={src} target='_blank' rel="noreferrer">
         <button className='p-2 border-2 border-solid border-black rounded-lg dark:border-white hover:bg-[#121212] hover:text-white dark:hover:bg-[#ede2e2] dark:hover:text-black'>
           {name}
         </button>
--- src/components/ui/button/Livelink.tsx
+++ src/components/ui/button/Livelink.tsx
@@ -1,7 +1,7 @@
 export const Livelink = ({ src, name }: any) => {
   return (
     <div>
-      <a href={src} target='_blank'>
+      <a href={src} target='_blank' rel="noreferrer">
         <button className='p-2 border-2 border-solid text-white rounded-lg bg-[#121212] dark:bg-[#ede2e2] dark:text-black'>
           {name}
         </button>

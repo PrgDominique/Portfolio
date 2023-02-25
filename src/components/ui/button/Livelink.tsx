158931760444b67f22965cabf8b0e0730c4f0829
PrgDominique <dominiquesimbillo10@gmail.com>
2023-02-25 19:59:54 +0800

    added EyeIcon in Live Link

--- src/components/ui/button/Livelink.tsx
+++ src/components/ui/button/Livelink.tsx
@@ -1,9 +1,11 @@
+import { EyeIcon } from '../Icon/EyeIcon'
+
 export const Livelink = ({ src, name }: any) => {
   return (
     <div>
       <a href={src} target='_blank' rel="noreferrer">
-        <button className='p-2 border-2 border-solid text-white rounded-lg bg-[#121212] dark:bg-[#ede2e2] dark:text-black'>
-          {name}
+        <button className='p-2 border-2 border-solid text-white rounded-lg bg-[#121212] dark:bg-[#ede2e2] dark:text-black flex gap-2'>
+         <EyeIcon /> {name}
         </button>
       </a>
     </div>
